6b2bb2dd02e7997c7903ecca1111ea315add0503
Théo Lacerda <theolacerda@gmail.com>
2025-10-01 08:58:47 -0300

    uptade

--- Exercicios/Ex-2.py
+++ Exercicios/Ex-2.py
@@ -1,2 +1,12 @@
-for i in range(30, 10025):  
-    print(i)  
+print('Você deseja um laço infinito ou com parada?')
+ope = input("Digite 'parada' ou 'infinito': ")
+
+if ope == 'parada':
+    for c in range(1, 11):
+        print(c)
+
+elif ope == 'infinito':
+    num = 0 
+    while True:
+        num += 1
+        print(num)
