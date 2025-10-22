9c33aaaa0e270155eacd9ce679c8d609cb006396
theolacerda-create <theolacerda@gmail.com>
2025-10-22 11:57:45 -0300

    Update Ex-5.py

--- Exercicios/Ex-5.py
+++ Exercicios/Ex-5.py
@@ -1,33 +1,54 @@
-usuario = input('Informe seu nome de usuário:')
+usuarios_registrados = []
 
-if (usuario != 'Admin' and usuario != 'Visitante' and usuario != 'Funcionario'): 
-    print('Usuário não cadastrado. Acesso negado.')
-    print('Sistema encerrado')
-if (usuario == 'Admin' or usuario == 'Visitante' or usuario == 'Funcionario'):
-    print('Olá', usuario, 'seja bem vindo ao sistema de acesso!')
-    senha = input('Para entrar informe sua senha:')
+voltar = 'trocar de usuario'  # Começa pedindo login
 
-while (senha != '196292021tT' and senha != '99999' and senha != '4241'):
-    print('Acesso negado')
-    senha = input('Senha incorreta. Tente novamente:')
+while voltar != 'sair':
+    if voltar == 'trocar de usuario':
+        usuario = input('Informe seu nome de usuário: ')
 
-if (usuario == 'Admin' and senha == '196292021tT'):
-    print('Acesso permitido')
-    print('Bem vindo Administrador')
-    print('O que deseja fazer em nosso sistema?')
-    manutenção = input('1- Manutenção do sistema 2- Ver usuários 3- Ver funcionarios 4- Sair do sistema: ')
+        if usuario not in ['Admin', 'Visitante', 'Funcionario']:
+            print('Usuário não cadastrado. Acesso negado.')
+            print('Sistema encerrado.')
+            break
+        else:
+            print('Olá', usuario, 'seja bem-vindo ao sistema de acesso!')
+            senha = input('Para entrar informe sua senha: ')
 
-elif (usuario == 'Funcionario' and senha == '4241'):
-    print('Acesso permitido')
-    print('Bem vindo Funcionário')
-    print('O que deseja verificar?')
-    funçoes = input('1- Ver tarefas 2- Ver agenda 3- Sair do sistema:')
+            while senha not in ['196292021tT', '99999', '4241']:
+                print('Acesso negado.')
+                senha = input('Senha incorreta. Tente novamente: ')
 
-elif (usuario == 'Visitante' and senha == '99999'):
-    print('Acesso permitido') 
-    print('Bem vindo Usuário')
-    print('Que ferramenta Deseja usar?')
-    ferramentas = input ('1- Acessar dados da sua empresa 2- Ver notícias 3- Sair do sistema: ')
+            usuarios_registrados.append(usuario)
+            print(f"Usuário '{usuario}' registrado com sucesso.\n")
+
+    # A partir daqui o usuário já está logado
+    if usuario == 'Admin' and senha == '196292021tT':
+        print('Acesso permitido.')
+        print('Bem-vindo Administrador.')
+        manutenção = input('1- Manutenção do sistema 2- Ver usuários 3- Ver funcionários 4- Sair do sistema: ')
+        if manutenção == '1':
+            print('Manutenção do sistema selecionada.')
+        elif manutenção == '2':
+            print('Lista de usuários registrados:', usuarios_registrados)
+        elif manutenção == '3':
+            print('Lista de funcionários: Funcionario1, Funcionario2, Funcionario3')
+        elif manutenção == '4':
+            print('Saindo do sistema.')
+
+    elif usuario == 'Funcionario' and senha == '4241':
+        print('Acesso permitido.')
+        print('Bem-vindo Funcionário.')
+        funcoes = input('1- Ver tarefas 2- Ver agenda 3- Sair do sistema: ')
+
+    elif usuario == 'Visitante' and senha == '99999':
+        print('Acesso permitido.')
+        print('Bem-vindo Usuário.')
+        ferramentas = input('1- Acessar dados da sua empresa 2- Ver notícias 3- Sair do sistema: ')
+
+    voltar = input('\nDeseja "trocar de usuario", continuar as "operações" ou "sair"?: ')
+    if voltar == 'sair':
+        print('Sistema encerrado.')
+
 
 
 
