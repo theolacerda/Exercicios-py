2791d0f3466f9457e46d837fbcc02410f15d354e
theolacerda-create <theolacerda@gmail.com>
2025-10-24 13:41:41 -0300

    uptade

--- piadas.py
+++ piadas.py
@@ -13,13 +13,13 @@ else:
 
 
 piadas = [
-    "Por que o livro de matemática se suicidou? Porque tinha muitos problemas!",
-    "O que o zero disse para o oito? Belo cinto!",
-    "Por que o computador foi ao médico? Porque pegou um vírus!",
-    "Qual é o cúmulo da paciência? Esperar o café esfriar!",
-    "Por que o jacaré tirou o jacarezinho da escola? Porque ele réptil de ano!",
-    "O que o tomate foi fazer no banco? Tirar extrato!",
-    "Por que a vaca foi para o espaço? Para se encontrar com a Via Láctea!"
+    "\nPor que o livro de matemática se suicidou? Porque tinha muitos problemas!",
+    "\nO que o zero disse para o oito? Belo cinto!",
+    "\nPor que o computador foi ao médico? Porque pegou um vírus!",
+    "\nQual é o cúmulo da paciência? Esperar o café esfriar!",
+    "\nPor que o jacaré tirou o jacarezinho da escola? Porque ele réptil de ano!",
+    "\nO que o tomate foi fazer no banco? Tirar extrato!",
+    "\nPor que a vaca foi para o espaço? Para se encontrar com a Via Láctea!"
 ]
 
 piadas_disponiveis = [p for p in piadas if p not in historico]
